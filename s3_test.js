688daed3823bc0b83b7f3306bc1ba3cdb7faa841
Rob Butcher <robbutcher2001@gmail.com>
2017-03-20 23:00:52 +0000

    Search S3

--- s3_test.js
+++ s3_test.js
@@ -1,13 +1,31 @@
-// Load the SDK for JavaScript
-var AWS = require('aws-sdk');
+// Load the S3 SDK for JavaScript
+var AWS = require('aws-sdk/clients/s3');
 // Load credentials and set region from JSON file
+// TODO: regen and move to DB and store in private container image
+// http://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-credentials.html
 AWS.config.loadFromPath('./s3_config.json');
 
 // Create S3 service object
-s3 = new AWS.S3({apiVersion: '2006-03-01'});
+s3 = new AWS.S3();
+
+var params = {Bucket: 'robertbutcher.co.uk-music-library'};
+
+var allKeys = [];
+function listAllKeys(marker, cb) {
+  s3.listObjects({Bucket: s3bucket, Marker: marker}, function(err, data) {
+    allKeys.push(data.Contents);
+
+    if (data.IsTruncated)
+      listAllKeys(data.NextMarker, cb);
+    else
+      cb();
+  });
+};
 
 // Call S3 to list current buckets
 s3.listBuckets(function(err, data) {
+  listAllKeys();
+  console.log("Recursive bucket", allKeys);
    if (err) {
       console.log("Error", err);
    } else {
