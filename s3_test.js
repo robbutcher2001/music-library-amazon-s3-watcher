6a825fd6696e712db62f6765f27f51523839f070
Rob Butcher <robbutcher2001@gmail.com>
2017-03-21 12:43:26 +0000

    Recusing promise

--- s3_test.js
+++ s3_test.js
@@ -10,19 +10,21 @@ AWS.config.loadFromPath('./s3_config.json');
 // Create S3 service object
 s3 = new AWS.S3();
 
-var allKeys = [];
-
 var s3MusicService = {};
 
-s3MusicService.getAllS3Tracks = function(artist) {
+s3MusicService.getAllS3Tracks = function() {
     return new Promise(function(resolve, reject) {
+        var allKeys = [];
         var params = {
-            Bucket: 'robertbutcher.co.uk-music-library'
+            Bucket: 'robertbutcher.co.uk-music-library',
+            MaxKeys: '10'
         };
+        recursiveFetchS3Keys();
+
         function recursiveFetchS3Keys() {
-            s3.listObjects(params, function (err, data) {
+            s3.listObjectsV2(params, function (err, data) {
                 if (err) {
-                    console.log(err, err.stack); // an error occurred
+                    reject(err);
                 } else {
                     var contents = data.Contents;
                     contents.forEach(function (content) {
@@ -35,41 +37,20 @@ s3MusicService.getAllS3Tracks = function(artist) {
                         recursiveFetchS3Keys();
                     }
 
-                    resolve(allKeys);
+                    //resolve(allKeys);
                 }
             });
         };
+
+        resolve(allKeys);
     });
 };
 
 module.exports = s3MusicService;
 
 console.log("get files..");
-// listAllKeys();
 s3MusicService.getAllS3Tracks().then(function(id) {
     console.log(id);
+}).catch(function(error) {
+    console.error(error, error.stack);
 });
-
-function listAllKeys() {
-    s3.listObjects(params, function (err, data) {
-        if (err) {
-            console.log(err, err.stack); // an error occurred
-        } else {
-            var contents = data.Contents;
-            contents.forEach(function (content) {
-                allKeys.push(content.Key);
-            });
-
-            if (data.IsTruncated) {
-                params.ContinuationToken = data.NextContinuationToken;
-                console.log("get further list..");
-                listAllKeys();
-            }
-
-            console.log("Length: " + allKeys.length)
-            console.log(allKeys);
-        }
-    });
-};
-
-// console.log(allKeys);
